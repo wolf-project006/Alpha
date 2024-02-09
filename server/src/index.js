15710892321d5f819a3cca037ed1c0f546855b07
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-09 13:48:24 +0900

    chore: add a path for dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const cors = require("cors");
-const knex = require("../db/knex.js");
-const dotenv = require("dotenv");
+const express = require('express');
+const cors = require('cors');
+const knex = require('../db/knex.js');
+const dotenv = require('dotenv');
 
-dotenv.config();
+dotenv.config({
+  path: '../.env',
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// todos routes
-app.post("/", (req, res) => {});
+// articles routes
+app.post('/', (req, res) => {});
 
 //test
-app.get("/", (req, res) => {
-  res.send("IM NOT WORKING BROOOOOOOOOO!!!:(((");
+app.get('/', (req, res) => {
+  res.send('IM WORKING NOW :)');
 });
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("Server running, ⚡️🏃");
+  console.log('Server running, ⚡️🏃');
 });
