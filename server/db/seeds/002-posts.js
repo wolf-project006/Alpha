91c7bb7d6b8789b6091d8096f656e40219565e31
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-09 14:28:02 +0900

    fix: modify user_id

--- server/db/seeds/002-posts.js
+++ server/db/seeds/002-posts.js
@@ -12,7 +12,7 @@ exports.seed = async function (knex) {
       body: 'This is the first post body',
       createdAt: new Date().toISOString(),
       updateAt: new Date().toISOString(),
-      userId: 1,
+      userId: 3,
     },
     {
       slug: 'second-post',
@@ -21,7 +21,7 @@ exports.seed = async function (knex) {
       body: 'This is the secons post body',
       createdAt: new Date().toISOString(),
       updateAt: new Date().toISOString(),
-      userId: 2,
+      userId: 4,
     },
   ]);
 };
