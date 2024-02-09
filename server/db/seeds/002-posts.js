38d0159434a2220da15625e425d9a21809b36442
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-09 14:16:20 +0900

    fix: fix the newDate to match the precision

--- server/db/seeds/002-posts.js
+++ server/db/seeds/002-posts.js
@@ -3,25 +3,25 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  await knex("post_table").del();
-  await knex("post_table").insert([
+  await knex('post_table').del();
+  await knex('post_table').insert([
     {
-      slug: "first-post",
-      title: "First Post",
-      description: "This is the first post.",
-      body: "This is the first post body",
-      createdAt: new Date(),
-      updateAt: new Date(),
-      userId: 11,
+      slug: 'first-post',
+      title: 'First Post',
+      description: 'This is the first post.',
+      body: 'This is the first post body',
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+      userId: 1,
     },
     {
-      slug: "second-post",
-      title: "Second Post",
-      description: "This is the second post.",
-      body: "This is the secons post body",
-      createdAt: new Date(),
-      updateAt: new Date(),
-      userId: 12,
+      slug: 'second-post',
+      title: 'Second Post',
+      description: 'This is the second post.',
+      body: 'This is the secons post body',
+      createdAt: new Date().toISOString(),
+      updateAt: new Date().toISOString(),
+      userId: 2,
     },
   ]);
 };
