64939a7c322fccf7aae91d32ba2374626dede404
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-10 19:19:36 +0900

    feat: SignUp/login logic, ready to be connected

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,36 @@
 const { useState } = require('react');
 
 const Login = () => {
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    try {
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      const data = response.json();
+
+      if (response.ok) {
+        console.log('Login successfully');
+        // Store the token in localStorage or a state management solution
+        // Redirect to the authenticated part of your application
+      } else {
+        console.error('Invalid credentials:', data);
+        // handle failure
+      }
+    } catch (error) {
+      console.error('Error during login:', error.message);
+    }
   };
 
   return (
@@ -15,11 +38,11 @@ const Login = () => {
       <form className="login" onSubmit={handleSubmit}>
         <h3>Login</h3>
 
-        <label>Email</label>
+        <label>Username</label>
         <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
         <label>Password</label>
         <input
@@ -27,7 +50,7 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
-        <button>Sign up</button>
+        <button type="submit">Login</button>
       </form>
     </>
   );
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,35 @@
 const { useState } = require('react');
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      const response = await fetch('/signup', {
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        console.log('User created successfully');
+        // Redirect logic here
+      } else {
+        console.error('Error creating user:', data);
+      }
+    } catch (error) {
+      console.error('Error creating user', error.message);
+    }
 
     console.log(email, password);
   };
@@ -20,6 +44,12 @@ const Signup = () => {
           type="email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
+        />
+        <label>Username</label>
+        <input
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
         />
         <label>Password</label>
         <input
