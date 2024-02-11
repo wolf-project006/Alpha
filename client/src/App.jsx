2923bebd7c0a8c3df2afcfd42c1f561983727ddf
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-11 16:07:39 +0900

    chore: wrap the app in the AuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
+import Login from '../src/pages/Login';
+import Dashboard from './pages/Dashboard';
 import SinglePost from './component/SinglePost';
+import { AuthProvider } from './context/authContext';
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/posts/:postId" component={SinglePost} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" exact Component={Home} />
+            <Route path="/posts/:postId" component={SinglePost} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </>
   );
