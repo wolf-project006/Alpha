5eff6985e831dd93fe6e9eaf0fe981ee82f8ca1e
Kiarosh Moeini <kmoeini@gmail.com>
2024-02-11 19:10:51 +0900

    rich text editor working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
+import PostEdit from './component/PostEdit';
 
 function App() {
   return (
     <>
+      <h1>Text Editor</h1>
+      <PostEdit />
       <Router>
         <Routes>
           <Route path="/" exact Component={Home} />
