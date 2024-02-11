ed5e31bd0fd361f15179d8292acd6ddf25aacf17
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-11 15:40:04 +0900

    feat: post component, list and single post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
+import SinglePost from './component/SinglePost';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" exact Component={Home} />
+          <Route path="/posts/:postId" component={SinglePost} />
         </Routes>
       </Router>
     </>
