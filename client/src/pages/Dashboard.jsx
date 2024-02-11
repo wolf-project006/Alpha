e590a7e8f86b223ae0a00952f85ba2a066185130
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-11 16:16:47 +0900

    chore: add Auth path

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,22 @@
 import UserPosts from '../component/UserPosts';
 import UserCard from '../component/Usercard';
+import { useAuth } from '../context/authContext';
 
 const Dashboard = () => {
+  const { user, logout } = useAuth();
+
   return (
     <>
       <h1>i'm a Dashboard</h1>
-      <UserCard />
+      <div>
+        {user && (
+          <>
+            <UserCard user={user} />
+            <button onClick={logout}>Logout</button>
+          </>
+        )}
+      </div>
+
       <UserPosts />
     </>
   );
