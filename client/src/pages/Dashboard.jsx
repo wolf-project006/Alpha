59eca130672fa4c5e9d4b4bd934e5f273c6a5171
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-11 15:49:11 +0900

    feat: user name display for the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import UserPosts from '../component/UserPosts';
+import UserCard from '../component/Usercard';
 
 const Dashboard = () => {
   return (
     <>
       <h1>i'm a Dashboard</h1>
+      <UserCard />
       <UserPosts />
     </>
   );
