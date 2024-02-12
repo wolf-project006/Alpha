ad6c9da58bd677a0f2a889e3d8ecfce1158cb118
mylordkaz <128312170+mylordkaz@users.noreply.github.com>
2024-02-12 11:18:30 +0900

    Revert "rich text editor working"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/pages/Home';
-import PostEdit from './component/PostEdit';
 
 function App() {
   return (
     <>
-      <h1>Text Editor</h1>
-      <PostEdit />
       <Router>
         <Routes>
           <Route path="/" exact Component={Home} />
