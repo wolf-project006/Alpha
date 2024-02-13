ca47291c5a42a422ea312ff9e3b5ec927554c181
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-13 15:21:37 +0900

    feat: add link to backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('https://wolf-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
