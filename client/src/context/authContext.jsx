0d1d1d6baa95af660dc036b5d74a224995b8f5ec
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-13 19:03:44 +0900

    test

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useState } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
 
   const login = (userData) => {
     setUser(userData);
