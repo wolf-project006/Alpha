9c767d904337239b5b6e23268bfc61f1e6dab8bd
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-13 10:41:49 +0900

    fix: fix the path to the signup

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <Link
-                to="sign-up"
+                to="/sign-up"
                 className="nav-links-mobile"
                 onClick={closeMobileMenu}
               >
