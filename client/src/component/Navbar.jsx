46cf94c095e6bd3c5faee1352a44de834861749a
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-13 10:30:56 +0900

    fix: className on the link

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,70 +1,78 @@
-import React, {useState, useEffect} from "react";
-import { Link } from "react-router-dom";
-import { Button } from "./Button";
-import "../styles/Navbar.css"
-import Signup from "../pages/SignUp";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import '../styles/Navbar.css';
+import Signup from '../pages/SignUp';
 
 const Navbar = () => {
-  const [click , setClick] = useState(false)
-  const [button, setButton] = useState(true)
-  const [showSignup, setShowSignup] = useState(false)
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const [showSignup, setShowSignup] = useState(false);
 
-  const handleClick = () => setClick(!click)
-  const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-  const showButton = ()=> {
-    if(window.innerWidth <= 960) {
-      setButton(false)
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
     } else {
-      setButton(true)
+      setButton(true);
     }
-  }
+  };
 
   const handleSignupClick = () => {
-    closeMobileMenu()
-    setShowSignup(!showSignup)
-  }
-
+    closeMobileMenu();
+    setShowSignup(!showSignup);
+  };
 
   useEffect(() => {
-    showButton()
-  },[])
+    showButton();
+  }, []);
 
-  window.addEventListener('resize', showButton)
+  window.addEventListener('resize', showButton);
 
   return (
     <>
-  <nav className="navbar">
-  <div className="navbar-container">
-    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}> Team Wolfie </Link>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            {' '}
+            Team Wolfie{' '}
+          </Link>
 
-      <div className="menu-icon" onClick={handleClick}> 
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-      </div>
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item">
-          <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-            Services
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-            product
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="sign-up" class="nav-links-mobile" onClick={closeMobileMenu}>Sign Up</Link>
-        </li>
-      </ul>
-      {button && <Button buttonStyle='btn--outline' >SIGN UP</Button>}
-      {showSignup && <Signup/>}
-  </div>
-</nav>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                product
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+          {showSignup && <Signup />}
+        </div>
+      </nav>
     </>
   );
 };
