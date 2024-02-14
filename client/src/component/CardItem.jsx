1fa0279e7503d958b5a68d95063639f01b666942
Kiarosh Moeini <kmoeini@gmail.com>
2024-02-14 10:29:10 +0900

    css styles updated and images added

--- client/src/component/CardItem.jsx
+++ client/src/component/CardItem.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import wolfImg from "../img/worfie.jpg"
+import React from "react";
+import { Link } from "react-router-dom";
+import art1 from "../img/art1.jpg";
 
 function CardItem() {
   return (
     <>
-      <li className='cards__item'>
+      <li className="cards__item">
         <Link className="cards__item__link">
           <figure className="cards__item__pic-wrap" data-category="Art">
-            <img className="cards__item__img" src={wolfImg} alt="Wolf" />
+            <img className="cards__item__img" src={art1} alt="art2" />
           </figure>
           <div className="cards__item__info">
-            <h5 className="cards__item__text">Put the description here!</h5>
+            <h5 className="cards__item__text">
+              The intersection of art and technology{" "}
+            </h5>
+            <p className="cards__item__text">
+              {" "}
+              From digital art to interactive installations, this piece explores
+              how modern artists are using technology...{" "}
+            </p>
           </div>
         </Link>
       </li>
     </>
-  )
+  );
 }
 
-export default CardItem
+export default CardItem;
