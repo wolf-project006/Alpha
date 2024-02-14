d55d0cab517e4a8de2e34c00e86939f9611b9ce8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-02-14 10:38:40 +0900

    fix navbar html

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -39,33 +39,27 @@ const Navbar = () => {
 
   return (
     <>
-
-
-  <nav className="navbar">
-  <div className="navbar-container">
-    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}> Artoholic </Link>
-
-      <div className="menu-icon" onClick={handleClick}> 
-        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-      </div>
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item">
-          <Link to='/' className="nav-links" onClick={closeMobileMenu}>
-            Home
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            {' '}
+            Artoholic{' '}
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-            Services
 
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/dashboard" className="nav-links" onClick={closeMobileMenu}>
-            Dashboard
-
-          </Link>
-          <ul>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
             <li className="nav-item">
               <Link
                 to="/dashboard"
@@ -75,31 +69,42 @@ const Navbar = () => {
                 Dashboard
               </Link>
             </li>
-            <li className="nav-item">
-              <Link
-                to="/sign-up"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/login"
-                className="nav-links-mobile"
-                onClick={closeMobileMenu}
-              >
-                Login
-              </Link>
-            </li>
-          </ul>
-          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
-          {button && (
-            <LoginButton buttonStyle="btn--outline">LOGIN</LoginButton>
-          )}
+            <ul>
+              <li className="nav-item">
+                <Link
+                  to="/dashboard"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/login"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Login
+                </Link>
+              </li>
+            </ul>
+            {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+            {button && (
+              <LoginButton buttonStyle="btn--outline">LOGIN</LoginButton>
+            )}
 
-          {showSignup && <Signup />}
+            {showSignup && <Signup />}
+          </ul>
         </div>
       </nav>
     </>
